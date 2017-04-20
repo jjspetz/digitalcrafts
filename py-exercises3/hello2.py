3732803cfa492fb4250acf776b3a24a341951ecf
JJ <jj.spetseris@yahoo.com>
2017-04-20 11:36:53 -0500

    changed hello2.py sys.argv to use of argparse

--- py-exercises3/hello2.py
+++ py-exercises3/hello2.py
@@ -2,10 +2,16 @@
 
 # This is a simple function that says hello using a command-line argument
 
-import sys
+import argparse
+
+# formats the arguments for argparse
+parser = argparse.ArgumentParser()
+parser.add_argument('username', metavar='name', type=str, nargs=1,
+                    help="Enter a name so the computer can say hello")
 
 def hello(name):
     print("Hello, {}!".format(name))
 
 if __name__ == "__main__":
-    hello(sys.argv[1])
+    args = parser.parse_args()
+    hello(args.username[0])
