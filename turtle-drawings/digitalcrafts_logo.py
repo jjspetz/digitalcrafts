7badaa809039917ef08903c717a49312bfea5fe8
JJ <jj.spetseris@yahoo.com>
2017-04-21 15:17:48 -0500

    logo finished

--- turtle-drawings/digitalcrafts_logo.py
+++ turtle-drawings/digitalcrafts_logo.py
@@ -8,10 +8,12 @@ from math import sqrt
 
 t.speed(0)
 
-COLORS1 = ()
-COLORS2 = ()
+COLORS1 = ("#7BC242", "#5B893E", "#7BC242", "#447764", "#1466A2", "#1D8DCD",
+ "#0D6FB8", "#1466A2", "#1F8CCD", "#1682B3")
+COLORS2 = ("#F89F1F", "#F47820", "#F89F1F", "#C28254", "#949597", "#BCBDBF",
+ "#A6AAAB", "#949597", "#A6AAAB", "#6E6D72")
 
-def print_half(factor=True):
+def print_half(colors, factor=True):
 
     #
     if factor:
@@ -23,7 +25,7 @@ def print_half(factor=True):
     t.up()
     t.bk(250 * f)
     t.lt(90)
-    t.fd(35 * f)
+    t.fd(32 * f)
 
     # add my shapes
     t.addshape("trapezoid", ((0, 0), (34 * f, 60 * f), (86 * f, 60 * f), (120 * f, 0)))
@@ -64,7 +66,7 @@ def print_half(factor=True):
 
     # prints a trapezoid
     t.shape("trapezoid")
-    t.color("#7BC242")
+    t.color(colors[0])
     t.stamp()
 
     # move to next local
@@ -72,7 +74,7 @@ def print_half(factor=True):
     t.fd(trap_move)
 
     # print quadrahedrial
-    t.color("#5B893E")
+    t.color(colors[1])
     t.seth(90)
     t.shape("quadrahedrial")
     t.stamp()
@@ -82,7 +84,7 @@ def print_half(factor=True):
     t.seth(0) # goes east
     t.fd(sTri_move) # calculates distance to move
     t.seth(270)
-    t.color("#7BC242")
+    t.color(colors[2])
     t.stamp()
 
     # move to bottom of triangle
@@ -92,7 +94,7 @@ def print_half(factor=True):
     t.fd((sTri_move/ 2))
 
     # print big triangle
-    t.color("#447764")
+    t.color(colors[3])
     t.seth(90)
     t.shape("bTri")
     t.stamp()
@@ -102,7 +104,7 @@ def print_half(factor=True):
     t.fd(bTri_move)
 
     # print big triangle
-    t.color("#1466A2")
+    t.color(colors[4])
     t.seth(270)
     t.shape("bTri")
     t.stamp()
@@ -114,7 +116,7 @@ def print_half(factor=True):
 
     # print trapezoid
     t.seth(270)
-    t.color("#1D8DCD")
+    t.color(colors[5])
     t.shape("trapezoid")
     t.stamp()
 
@@ -126,7 +128,7 @@ def print_half(factor=True):
     # print bottom left quadrahedrial
     t.shape("reverse_quad")
     t.seth(90)
-    t.color("#0D6FB8")
+    t.color(colors[6])
     t.stamp()
 
     # move to next point
@@ -136,7 +138,7 @@ def print_half(factor=True):
     t.fd(sTri_move_y)
 
     # reprint blue triangle
-    t.color("#1466A2")
+    t.color(colors[7])
     t.seth(270)
     t.shape("bTri")
     t.stamp()
@@ -148,7 +150,7 @@ def print_half(factor=True):
     t.fd(bTri_move_y - sTri_move_y)
 
     # print small triangle
-    t.color("#1F8CCD")
+    t.color(colors[8])
     t.seth(90)
     t.shape("sTri")
     t.stamp()
@@ -159,13 +161,14 @@ def print_half(factor=True):
     t.fd(sTri_move/2)
 
     # print last big triangle
-    t.color("#1482B3")
+    t.color(colors[9])
     t.shape("bTri")
     t.seth(270)
     t.stamp()
 
 if __name__ == "__main__":
-    print_half(True)
+    print_half(COLORS1)
     t.home()
-    print_half(False)
+    print_half(COLORS2, False)
+    t.bgcolor("#223E4A")
     t.mainloop()
