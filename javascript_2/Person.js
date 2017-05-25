44ae352d16c8484ad8de3be59dafedf1941a55a8
JJ <jj.spetseris@yahoo.com>
2017-05-25 10:48:51 -0500

    person exe added

---
+++ javascript_2/Person.js
@@ -0,0 +1,20 @@
+// rewrite old style function class as an E6JS class
+
+class Person {
+  constructor(name) {
+    this.name = name;
+    this.friends = [];
+  }
+
+  addFriend(friend) {
+    this.friends.push(friend);
+  }
+
+  createGreeting(other) {
+    return 'Yo ' + other.name + '! from ' + this.name + '.';
+  }
+
+  greet(other) {
+    console.log(this.createGreeting(other));
+  }
+}
