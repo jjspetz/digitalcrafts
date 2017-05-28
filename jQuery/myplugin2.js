e3c1dcef410bd7bda9d91abc04b846e087959dc6
JJ <jj.spetseris@yahoo.com>
2017-05-28 15:47:04 -0500

    2nd plug in works for images but breaksother things

--- jQuery/myplugin2.js
+++ jQuery/myplugin2.js
@@ -1,15 +1,13 @@
-// changes (img) to fullscreen on hover.
+// changes (img) bigger on hover.
 
 $.fn.big = function() {
   var initialWidth;
   this.hover(function(event) {
-    $(event.target).css('border-radius', '5px');
     initialWidth = $(event.target).css('width');
-    $(event.target).css('width', '95%');
-    $(event.target).css('margin', '2em auto');
+    console.log(initialWidth);
+    $(event.target).css('width', '100%');
   }, function(event) {
-    $(event.target).css('border-radius', '0%');
-    $(event.target).css('width', initialwidth);
+    $(event.target).css('width', initialWidth);
   }
 );
 }
