2be3ae7be52c58a2618ff6fd38b5d3d3c1e2a721
JJ <jj.spetseris@yahoo.com>
2017-05-30 13:45:24 -0500

    Node.js exercise one: read a file

---
+++ js3/cap_file.js
@@ -0,0 +1,13 @@
+// Node.js exercise one: read a file
+
+var fs = require('fs');
+var file = 'file1.txt';
+
+fs.readFile(file, function(error, buffer) {
+  if (error) {
+    console.log(error.message);
+    return;
+  }
+  console.log('filename: ' + file);
+  console.log(buffer.toString());
+});
