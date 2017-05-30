79060157a6a00d5549bd47abaab7ef836a54441a
JJ <jj.spetseris@yahoo.com>
2017-05-30 12:53:53 -0500

    js3 initial commit

---
+++ js3/funcRewrite.js
@@ -0,0 +1,23 @@
+/*
+  REWRITE these functions using callbacks
+
+  function add(x, y) {
+  var result = x + y;
+  return result;
+}
+
+function subtract(x, y) {
+  return x - y;
+}
+
+function greeting(person) {
+  return 'Hola, ' + person + '!';
+}
+
+function product(numbers) {
+  return numbers.reduce(function(a, b) {
+    return a * b;
+  }, 1);
+}
+
+*/
