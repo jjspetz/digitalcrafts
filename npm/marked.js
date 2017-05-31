22c527377d041a26d9accb9eb9dd463ef6ddb0b6
JJ <jj.spetseris@yahoo.com>
2017-05-31 11:49:54 -0500

    marked exer added

---
+++ npm/marked.js
@@ -0,0 +1,17 @@
+// parcitce using marked from npm
+
+var marked = require('marked');
+
+marked.setOptions({
+  renderer: new marked.Renderer(),
+  gfm: true,
+  tables: true,
+  breaks: false,
+  pedantic: false,
+  sanitize: true,
+  smartLists: true,
+  smartypants: false
+});
+
+console.log(marked('# Houston-Hackathon'));
+console.log(marked('I am using __markdown__.'));
