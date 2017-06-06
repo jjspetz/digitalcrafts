1c7a870ed1f12d97d2ea0efe2a99263b4df0b3c5
JJ <jj.spetseris@yahoo.com>
2017-06-06 09:31:07 -0500

    removing node_modules

--- express/app.js
+++ express/app.js
@@ -9,6 +9,13 @@ app.get('/', function(request, response) {
   response.send('Hello World!');
 });
 
+app.get('/about', function(request, response) {
+  response.send('About Me');
+});
+
+app.get('/projects', function(request, response) {
+  response.send('My Projects');
+});
 // server port
 app.listen(8000, function() {
   console.log('listening on port 8000')
