33fdd9e6119b72b365650c95d9c412cdc4a08efa
JJ <jj.spetseris@yahoo.com>
2017-06-06 09:42:57 -0500

    more routes added

--- express/app.js
+++ express/app.js
@@ -8,14 +8,23 @@ var app = express();
 app.get('/', function(request, response) {
   response.send('Hello World!');
 });
-
 app.get('/about', function(request, response) {
   response.send('About Me');
 });
-
 app.get('/projects', function(request, response) {
   response.send('My Projects');
 });
+// get URL paramaters
+app.get('/post/:slug', function(request, response) {
+  var slug = request.params.slug;
+  response.send('Post About: ' + slug);
+});
+// get parameters
+app.get('/hello', function(request, response) {
+  var name = request.query.name || 'World;'
+  response.send('Hello ' + name);
+});
+
 // server port
 app.listen(8000, function() {
   console.log('listening on port 8000')
