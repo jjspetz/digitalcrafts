e15b0ff234b6998ab2fe317652d6011e11d27100
JJ <jj.spetseris@yahoo.com>
2017-06-12 09:32:00 -0500

    rectange appears in center of the screen

--- canvas/drawRect.js
+++ canvas/drawRect.js
@@ -1,4 +1,13 @@
+// setup canvas
 var canvas = document.querySelector('canvas');
 var ctx = canvas.getContext('2d');
+
+
+// set canvas width and height to width and height of screen
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+
+// draw rectangle in center of screen
 ctx.fillStyle = 'red';
-ctx.fillRect(50, 50, 100, 200);
+ctx.fillRect((canvas.width/2 - 50), (canvas.height/2 - 100), 100, 200);
