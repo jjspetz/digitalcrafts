180870361477423581548ce8c2139a22f3a01a0e
JJ <jj.spetseris@yahoo.com>
2017-06-12 10:37:42 -0500

    2 circles inside of each other

--- canvas/circles.js
+++ canvas/circles.js
@@ -14,14 +14,25 @@ window.addEventListener('resize', function() {
   canvas.width = window.innerWidth;
   canvas.height = window.innerHeight;
   circle();
+  insideCircle();
 });
 
 
 // draw a circle in center of screen
 function circle() {
+  ctx.beginPath();
   ctx.fillStyle = 'blue';
   ctx.arc((canvas.width/2), (canvas.height/2), 50, 0, Math.PI * 2, false);
   ctx.fill();
 }
 
+// draws a second inner circle;
+function insideCircle() {
+  ctx.beginPath();
+  ctx.fillStyle = 'red';
+  ctx.arc((canvas.width/2), (canvas.height/2), 25, 0, Math.PI * 2, false);
+  ctx.fill();
+}
+
 circle();
+insideCircle();
