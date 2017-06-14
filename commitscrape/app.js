c86070d73aa23a76931b51ea7068c097ed94dcfb
JJ <jj.spetseris@yahoo.com>
2017-06-14 11:39:30 -0500

    working app for scrapping my github commits

--- commitscrape/app.js
+++ commitscrape/app.js
@@ -1,9 +1,25 @@
-// testing our github commit scrapper
+// testing a github commit scrapper
 
 const express = require('express');
 const app = express();
 const contributions = require('github-yearly-contributions');
 
+var apicache = require('apicache');
+var cache = apicache.middleware;
+
+var commitNum;
+
 contributions('jjspetz', function(err, amount){
-  console.log('Contributions this year = ' + amount);
+  commitNum = amount;
+});
+
+app.get('/api', cache('1 day'), function(request, response) {
+  response.json(
+    {commits: commitNum}
+  );
+});
+
+// server port
+app.listen(8000, function() {
+  console.log('listening on port 8000')
 });
