9c3493da1ac430d3119eef772c6bdb1c289f6a58
JJ <jj.spetseris@yahoo.com>
2017-06-28 11:42:22 -0500

    AppBar added

---
+++ contacts-app/src/contacts.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import AppBar from 'material-ui/AppBar';
+import './contacts.css';
+
+const Contacts = () => (
+  <div className='main'>
+    <AppBar title="Contacts" />
+    <div className='content'>
+
+    </div>
+  </div>
+);
+
+export default Contacts
