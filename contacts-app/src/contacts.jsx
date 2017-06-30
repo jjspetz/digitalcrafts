285e682d00b89e90dff99e56681cf79be736e160
JJ <jj.spetseris@yahoo.com>
2017-06-30 11:37:00 -0500

    localStorage is fully handling the saving and deleting of contacts

--- contacts-app/src/contacts.jsx
+++ contacts-app/src/contacts.jsx
@@ -8,6 +8,7 @@ class Contacts extends Component {
   constructor() {
     super();
     this.state =  {
+      id: '',
       name: '',
       firstName: '',
       lastName: '',
@@ -25,20 +26,27 @@ class Contacts extends Component {
 
   // set contacts to local storage if any
   getLocalStorage() {
-    for (var i=0; i<localStorage.length; i++) {
+    // clear out current state
+    //this.setState({ contacts: [] });
+    this.state.contacts = [];
+
+    Object.keys(localStorage).forEach((value) => {
       this.state.contacts.push({
-        name: JSON.parse(localStorage['id'+i]).name,
-        firstName: JSON.parse(localStorage['id'+i]).firstName,
-        lastName: JSON.parse(localStorage['id'+i]).lastName,
-        email: JSON.parse(localStorage['id'+i]).email,
-        phone: JSON.parse(localStorage['id'+i]).phone,
-        address: JSON.parse(localStorage['id'+i]).address,
-        city: JSON.parse(localStorage['id'+i]).city,
-        state: JSON.parse(localStorage['id'+i]).state,
-        zip: JSON.parse(localStorage['id'+i]).zip,
+          id: JSON.parse(localStorage[value]).id,
+          name: JSON.parse(localStorage[value]).name,
+          firstName: JSON.parse(localStorage[value]).firstName,
+          lastName: JSON.parse(localStorage[value]).lastName,
+          email: JSON.parse(localStorage[value]).email,
+          phone: JSON.parse(localStorage[value]).phone,
+          address: JSON.parse(localStorage[value]).address,
+          city: JSON.parse(localStorage[value]).city,
+          state: JSON.parse(localStorage[value]).state,
+          zip: JSON.parse(localStorage[value]).zip,
       })
-    }
-    this.setState({ contacts: this.sortContacts(this.state.contacts) });
+    });
+
+    this.state.contacts = this.sortContacts(this.state.contacts);
+    this.setState({ contacts:  this.state.contacts});
   }
 
 
@@ -53,13 +61,8 @@ class Contacts extends Component {
   }
   handleDelete(event, contact) {
     // remove from local storage
-    localStorage.removeItem('contact');
-    // remove from state
-    this.state.contacts = this.state.contacts.splice(
-      this.state.contacts.indexOf(this.state.contacts.key),
-      1
-    );
-    this.setState({ contacts: this.state.contacts })
+    localStorage.removeItem(contact.id);
+    this.getLocalStorage();
   }
 
   expand(event, contact) {
@@ -73,26 +76,28 @@ class Contacts extends Component {
       let x = a.lastName; let y = b.lastName;
       return ((x < y) ? -1 : (x > y) ? 1 : 0);
     });
+    return array;
   }
 
   addSubmitToContact = () => {
     // pushed info to state
-    this.state.contacts.push({
-      name: this.state.lastName + ', ' + this.state.firstName.slice(),
-      firstName: this.state.firstName,
-      lastName: this.state.lastName,
-      email: this.state.email,
-      phone: this.state.phone,
-      address: this.state.address,
-      city: this.state.city,
-      state: this.state.state,
-      zip: this.state.zip,
-      isOpen: false,
-    })
+    // this.state.contacts.push({
+    //   id: '',
+    //   name: this.state.lastName + ', ' + this.state.firstName.slice(),
+    //   firstName: this.state.firstName,
+    //   lastName: this.state.lastName,
+    //   email: this.state.email,
+    //   phone: this.state.phone,
+    //   address: this.state.address,
+    //   city: this.state.city,
+    //   state: this.state.state,
+    //   zip: this.state.zip,
+    //   isOpen: false,
+    // })
     // puts info on local storage
-    let id = 'id' + localStorage.length;
+    let id = (parseInt(Object.keys(localStorage)[localStorage.length-1]) + 1) || 0;
     localStorage[id] = JSON.stringify({
-      id: localStorage.length,
+      id: id,
       name: this.state.lastName + ', ' + this.state.firstName.slice(),
       firstName: this.state.firstName,
       lastName: this.state.lastName,
@@ -104,8 +109,7 @@ class Contacts extends Component {
       zip: this.state.zip,
       isOpen: false,
     })
-
-    this.setState({ contacts: this.sortContacts(this.state.contacts) });
+    this.getLocalStorage();
   }
 
   render() {
@@ -175,10 +179,10 @@ class Contacts extends Component {
                 </div>
               )}
             )}
+          </div>
         </div>
-      </div>
     );
   }
 }
 
-export default Contacts
+export default Contacts;
