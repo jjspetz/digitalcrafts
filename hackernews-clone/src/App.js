e8473c6081305fbed52eee4bb3a6ae520cb187d4
JJ <jj.spetseris@yahoo.com>
2017-07-10 15:35:37 -0500

    on change top500 gets new 500 objects

--- hackernews-clone/src/App.js
+++ hackernews-clone/src/App.js
@@ -2,10 +2,20 @@ import React, { Component } from 'react';
 import './App.css';
 import database from './fb-config';
 
+var top500 = [];
+
 database.ref('/v0/topstories')
   .on('value', function(articles) {
-    console.log(articles.val());
+    articles.val().forEach(function(item) {
+      database.ref('/v0/item/' + item)
+        .once('value').then(function(data) {
+          top500.push(data.val());
+        })
+    })
+    console.log(top500);
   });
+
+
 
 class App extends Component {
   render() {
@@ -22,6 +32,14 @@ class App extends Component {
             <a> submit</a>
           </h6>
         </header>
+        <main>
+          <ol>
+            <li></li>
+          </ol>
+        </main>
+        <footer>
+
+        </footer>
       </div>
     );
   }
