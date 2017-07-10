e85d885ced7b94336b18a54bda4ec051bec160f2
JJ <jj.spetseris@yahoo.com>
2017-07-10 15:05:50 -0500

    added basic header

--- hackernews-clone/src/App.js
+++ hackernews-clone/src/App.js
@@ -11,7 +11,17 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-
+        <header className='header'>
+          <h5>Hacker News </h5>
+          <h6>
+            <a> new</a> |
+            <a> comments</a> |
+            <a> show</a> |
+            <a> ask</a> |
+            <a> jobs</a> |
+            <a> submit</a>
+          </h6>
+        </header>
       </div>
     );
   }
