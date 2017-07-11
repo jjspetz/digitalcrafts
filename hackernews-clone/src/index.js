da084005b83bf169797d30cfac7a12000e80b6db
JJ <jj.spetseris@yahoo.com>
2017-07-10 22:04:27 -0500

    url close to correct with convert to string hack

--- hackernews-clone/src/index.js
+++ hackernews-clone/src/index.js
@@ -15,8 +15,7 @@ database.ref('/v0/topstories')
         .once('value').then(function(data) {
           // converts time to hours ago
           let time = Math.floor((new Date().getTime()/1000 - data.val().time)/3600);
-          // let url = data.val().url;
-          // url = url.split('/');
+          let url = (data.val().url + 'stupidhack').split('\/');
           top500.push({
             id: data.val().id,
             title: data.val().title,
@@ -25,7 +24,7 @@ database.ref('/v0/topstories')
             descendants: data.val().descendants,
             kids: data.val().kids,
             time: time,
-            url: data.val().url
+            url: url[2]
           });
           loadPage();
         });
